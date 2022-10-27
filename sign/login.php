869c858b3c4c82b0d806a99e3e2dcfc42d63508e
GabeLove <gabriel_ballesteros98@hotmail.com>
2022-10-26 20:43:36 -0500

    creacion de login desde cero

--- sign/login.php
+++ sign/login.php
@@ -1,31 +1,3 @@
-<?php
-
-include "../config/config.php";
-error_reporting(0);
-session_start();
-
-if(isset($_SESSION["username"])){
-	header("Location: index.php");
-}
-
-if(isset($_POST["submit"])){
-	$nombre=$_POST["nombre"];
-	$correo=$_POST["correo"];
-	$contrasena=md5($_POST["contraseña"]);
-	$rcontrasena=md5($_POST["rcontraseña"]);
-
-	if($contrasena){
-
-	}
-}
-?>
-
-
-<?php
-
-include"../header.php";
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -33,67 +5,28 @@ include"../header.php";
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-    <link rel="stylesheet" href="../Css/styleslogin.css">
+    <link rel="stylesheet" href="../Css/styles.css">
     <title>Login</title>
 </head>
 <body>
+<?php
+  
+  include "../header.php"
+  
+  ?>
+<div class="inicio">
+  <h1 class="welcome">Bienvenido, Inicia Sesion!</h1>
 
-	<section class="forms-section">
-  <h1 class="section-title">Bienvenido!</h1>
-  <div class="forms">
-    <div class="form-wrapper is-active">
-      <button type="button" class="switcher switcher-login">
-	  Inicia Sesión
-        <span class="underline"></span>
-      </button>
-      <form class="form form-login">
-        <fieldset>
-          
-          <div class="input-block">
-            <label for="login-email">Correo</label>
-            <input id="login-email" type="email" required>
-          </div>
-          <div class="input-block">
-            <label for="login-password">Contraseña</label>
-            <input id="login-password" type="password" required>
-          </div>
-        </fieldset>
-        <button type="submit" class="btn-login">Iniciar Sesión</button>
-      </form>
-    </div>
-    <div class="form-wrapper">
-      <button type="button" class="switcher switcher-signup">
-        Registro
-        <span class="underline"></span>
-      </button>
-      <form class="form form-signup">
-        <fieldset>
-          
-          <div class="input-block">
-            <label for="signup-email">Nombre</label>
-            <input id="signup-name" type="text" required>
-          </div>
-		 
-		  <div class="input-block">
-            <label for="signup-email">Correo</label>
-            <input id="signup-email" type="email" required>
-          </div>
-          <div class="input-block">
-            <label for="signup-password">Contraseña</label>
-            <input id="signup-password" type="password" required>
-          </div>
-          <div class="input-block">
-            <label for="signup-password-confirm">Confirmar contraseña</label>
-            <input id="signup-password-confirm" type="password" required>
-          </div>
-        </fieldset>
-        <button type="submit" class="btn-signup">Continue</button>
-      </form>
-    </div>
+	<form action="login.php" method="post" class="formulario">
+    <input type="text" name="email" placeholder="Introduce tu correo">
+
+    <input type="text" name="password" placeholder="Introduce tu contrasena">
+
+    <input type="submit" value="Registrarse">
+
+
+  </form>
   </div>
-</section>
-
-<script src="../js/script.js"></script>
 
 </body>
 </html>
